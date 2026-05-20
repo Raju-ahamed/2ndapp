e661a6ecb380585eb5b4001bac118350858c90a2
Raju-ahamed <rajuahamed7320@gmail.com>
2026-05-20 16:08:44 +0600

    3rd repo

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express')
+const phones = require('./phones.json')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send(`'Hello World!'
-        <div>btn</div>
+    res.send(`'Hello World!
+        my phone server start soon
         
         `)
 })
-
+app.get('/phones', (req, res) => {
+    res.send(phones);
+})
+app.get('/phones/:id', (req, res) => {
+const id = req.params.id;
+console.log("i ned data for id",id)
+})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
